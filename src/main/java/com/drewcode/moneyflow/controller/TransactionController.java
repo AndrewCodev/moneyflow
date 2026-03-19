26b2700c643f1f631892fb66e07c726cc220b0ac
AndrewCodev <andrewcodev@gmail.com>
2026-03-19 01:44:35 -0500

    Se realizan ajustes menoeres en el controller y se configura H2 para realizar pruebas como base de datos

--- src/main/java/com/drewcode/moneyflow/controller/TransactionController.java
+++ src/main/java/com/drewcode/moneyflow/controller/TransactionController.java
@@ -2,16 +2,16 @@ package com.drewcode.moneyflow.controller;
 
 import java.util.List;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.drewcode.moneyflow.entity.Transaction;
 import com.drewcode.moneyflow.service.TransactionService;
 
-@Controller
+@RestController
 @RequestMapping("/transaction")
 public class TransactionController {
 
@@ -21,7 +21,7 @@ public class TransactionController {
         this.service = service;
     }
 
-    @PostMapping("create")
+    @PostMapping
     public Transaction create(@RequestBody Transaction transaction) {
         return service.save(transaction);
     }
